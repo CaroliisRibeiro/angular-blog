8ed7079026f7cd7efa2ccb9081bb548c2a19df89
Carol Ribeiro <anacalinda@gmail.com>
2025-03-27 16:53:23 -0300

    Blog ElasCode home e paginas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,29 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "1",
+  "title": "Elas Code: Programando Mudanças, Inspirando Gerações",
+  "description": "O Elas Code é mais que uma iniciativa — é um movimento que transforma realidades por meio da tecnologia. Criado para incentivar e capacitar mulheres a ingressarem no universo da programação, o projeto promove inclusão, equidade e autonomia. Cada linha de código escrita por essas mulheres representa uma conquista pessoal e um passo coletivo rumo a um futuro mais justo, onde o protagonismo feminino na tecnologia deixa de ser exceção e se torna regra.",
+  "photoCover": "https://img.freepik.com/vetores-premium/mulher-programadora-na-ilustracao-do-processo-de-trabalho_275655-531.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "title": "TechFlora: Cultivando Talentos na Programação",
+    "description": "O curso TechFlora oferece uma trilha formativa acessível e acolhedora para mulheres que desejam florescer no universo da tecnologia.",
+    "photoCover":"https://spbancarios.com.br/sites/default/files/styles/medium_3/public/destaques/2025-03/site-programar-bolsa.jpg?itok=s8KnFcSJ"
+  },
+
+  {
+  "id": "3",
+  "title": "Mentoria Codifica+: Compartilhando Saberes",
+  "description": "O programa Codifica+ conecta mulheres a mentoras experientes para fortalecer redes de apoio e crescimento na carreira tech.",
+  "photoCover": "https://img.freepik.com/vetores-premium/comunidade-de-jovens-compartilhando-ideias-noticias_82574-10741.jpg?semt=ais_hybrid"
+},
+{
+    "id": "4",
+    "title": "ConectaTech 2025: Mulheres que Inspiram, Códigos que Transformam",
+    "description": "O ConectaTech 2025 é um evento gratuito e 100% online que reúne mulheres da tecnologia para compartilhar experiências, aprender em oficinas práticas e se conectar com mentoras incríveis. Participe desse movimento que celebra a força feminina no universo tech!",
+    "photoCover": "https://thumbs.dreamstime.com/b/videoconfer%C3%AAncia-grupo-de-pessoas-na-tela-do-computador-conversando-reuni%C3%A3o-virtual-conceito-vetor-comunica%C3%A7%C3%A3o-online-em-182279175.jpg"
+  
+}
+
 ]
