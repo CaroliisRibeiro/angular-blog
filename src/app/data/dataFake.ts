5f12ce23c9999ae173f01de3af297d9dac9d6917
Carol Ribeiro <anacalinda@gmail.com>
2025-04-04 06:35:12 -0300

    ajuste logo e texto descrição

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,29 +1,29 @@
 export const dataFake = [
   {
     "id": "1",
-  "title": "Elas Code: Programando Mudanças, Inspirando Gerações",
-  "description": "O Elas Code é mais que uma iniciativa — é um movimento que transforma realidades por meio da tecnologia. Criado para incentivar e capacitar mulheres a ingressarem no universo da programação, o projeto promove inclusão, equidade e autonomia. Cada linha de código escrita por essas mulheres representa uma conquista pessoal e um passo coletivo rumo a um futuro mais justo, onde o protagonismo feminino na tecnologia deixa de ser exceção e se torna regra.",
-  "photoCover": "https://img.freepik.com/vetores-premium/mulher-programadora-na-ilustracao-do-processo-de-trabalho_275655-531.jpg"
-  },
-  {
-    "id":"2",
-    "title": "TechFlora: Cultivando Talentos na Programação",
-    "description": "O curso TechFlora oferece uma trilha formativa acessível e acolhedora para mulheres que desejam florescer no universo da tecnologia.",
-    "photoCover":"https://spbancarios.com.br/sites/default/files/styles/medium_3/public/destaques/2025-03/site-programar-bolsa.jpg?itok=s8KnFcSJ"
+    "title": "Elas Code: Programando Mudanças, Inspirando Gerações",
+    "description": "O Elas Code é mais que uma iniciativa — é um movimento que transforma realidades por meio da tecnologia.Criado para incentivar e capacitar mulheres a ingressarem no universo da programação, o projeto promove inclusão, equidade e autonomia.\n\nCada linha de código escrita por essas mulheres representa uma conquista pessoal e um passo coletivo rumo a um futuro mais justo, onde o protagonismo feminino na tecnologia deixa de ser exceção e se torna regra.",
+    "photoCover": "https://img.freepik.com/vetores-premium/mulher-programadora-na-ilustracao-do-processo-de-trabalho_275655-531.jpg"
   },
 
   {
-  "id": "3",
-  "title": "Mentoria Codifica+: Compartilhando Saberes",
-  "description": "O programa Codifica+ conecta mulheres a mentoras experientes para fortalecer redes de apoio e crescimento na carreira tech.",
-  "photoCover": "https://img.freepik.com/vetores-premium/comunidade-de-jovens-compartilhando-ideias-noticias_82574-10741.jpg?semt=ais_hybrid"
-},
-{
+    "id": "2",
+    "title": "TechFlora: Cultivando Talentos na Programação",
+    "description": "O curso TechFlora oferece uma trilha formativa acessível e acolhedora para mulheres que desejam florescer no universo da tecnologia.\n\nPor meio de aulas práticas, mentorias e projetos colaborativos, as participantes desenvolvem habilidades técnicas e ampliam suas possibilidades de atuação no mercado.",
+    "photoCover": "https://spbancarios.com.br/sites/default/files/styles/medium_3/public/destaques/2025-03/site-programar-bolsa.jpg?itok=s8KnFcSJ"
+  },
+
+  {
+    "id": "3",
+    "title": "Mentoria Codifica+: Compartilhando Saberes",
+    "description": "O programa Codifica+ conecta mulheres a mentoras experientes para fortalecer redes de apoio e crescimento na carreira tech.\n\nAtravés de encontros online, trocas de experiências e acompanhamento individual, as participantes são orientadas a superar desafios, estabelecer metas e ampliar sua confiança profissional.",
+    "photoCover": "https://img.freepik.com/vetores-premium/comunidade-de-jovens-compartilhando-ideias-noticias_82574-10741.jpg?semt=ais_hybrid"
+  },
+
+  {
     "id": "4",
     "title": "ConectaTech 2025: Mulheres que Inspiram, Códigos que Transformam",
-    "description": "O ConectaTech 2025 é um evento gratuito e 100% online que reúne mulheres da tecnologia para compartilhar experiências, aprender em oficinas práticas e se conectar com mentoras incríveis. Participe desse movimento que celebra a força feminina no universo tech!",
+    "description": "O ConectaTech 2025 é um evento gratuito e 100% online que reúne mulheres da tecnologia para compartilhar experiências, aprender em oficinas práticas e se conectar com mentoras incríveis.\n\nParticipe desse movimento que celebra a força feminina no universo tech e fortalece a presença de mulheres em todas as áreas da inovação.",
     "photoCover": "https://thumbs.dreamstime.com/b/videoconfer%C3%AAncia-grupo-de-pessoas-na-tela-do-computador-conversando-reuni%C3%A3o-virtual-conceito-vetor-comunica%C3%A7%C3%A3o-online-em-182279175.jpg"
-  
-}
-
+  }
 ]
